3f4b4b69ba815eca4fc7098dd224de7c3c8d129f
Yamihoshi <a4arkane@gmail.com>
2017-04-13 16:54:48 +0200

    Character Select Grid + Font bug

--- src/GUI/controller/CharacterSelectionController.java
+++ src/GUI/controller/CharacterSelectionController.java
@@ -1,5 +1,6 @@
 package GUI.controller;
 
+import java.io.InputStream;
 import java.net.URISyntaxException;
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -10,6 +11,7 @@ import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.text.Font;
 
 public class CharacterSelectionController implements Initializable {
 
@@ -22,10 +24,16 @@ public class CharacterSelectionController implements Initializable {
     @FXML 
     private ImageView character_display_J2; 
     
+    @FXML
+    private Label font;
+    
   
     @Override  
     public void initialize(URL location, ResourceBundle resources) {  
     	try {
+    		InputStream fontStream = getClass().getResourceAsStream("/fonts/SSF4.ttf");
+    		Font bgFont = Font.loadFont(fontStream, 36);
+    		font.setFont(bgFont);
 			character_display_J1.setImage(new Image(getClass().getResource("/Character_Display/Ryu_J2.png").toURI().toString()));
 		} catch (URISyntaxException e) {
 			// TODO Auto-generated catch block
