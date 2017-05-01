de70a3810e59c8e02064856179a8b6da0e2771a0
Yamihoshi <Yamihoshi@users.noreply.github.com>
2017-05-02 00:14:58 +0200

    sprites alignement ratio

--- src/GUI/animations/Sprite.java
+++ src/GUI/animations/Sprite.java
@@ -4,19 +4,21 @@ import javafx.scene.image.Image;
 
 public class Sprite {
 
-	public static final int ref_position_Y = 430;
-	public static final int ref_position_X_left = 173;
+	public static final double ratio = 65.0/100.0;
 	
-	private int translate_X;
-	private int translate_Y;
+	public static final int ref_position_Y = (int) (ratio*430);
+	public static final int ref_position_X_left = (int) (ratio*173);
+	
+	private double translate_X;
+	private double translate_Y;
 	private int duration;
 	
 	private Image image;
 	
 	public Sprite(int posX, int posY,int duration, Image img,boolean reversed)
 	{
-		this.translate_X = posX;
-		this.translate_Y = posY;
+		this.translate_X = ratio*posX;
+		this.translate_Y = ratio*posY;
 		this.duration = duration;
 		
 		this.image = img;
@@ -33,12 +35,12 @@ public class Sprite {
 		return this.duration;
 	}
 	
-	public int getTranslate_X()
+	public double getTranslate_X()
 	{
 		return this.translate_X;
 	}
 	
-	public int getTranslate_Y()
+	public double getTranslate_Y()
 	{
 		return this.translate_Y;
 	}
